aa05409ccdd6f7ed19ea70349841071c0fef09fa
4018KShvaykoAS <supercilious_me@vk.com>
2023-06-06 13:40:28 +0300

    Sources

--- Lesson 4/test_main_page.py
+++ Lesson 4/test_main_page.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
     login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     login_link.click()
+
+def test_guest_can_go_to_login_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    browser.get(link) 
+    go_to_login_page(browser) 
