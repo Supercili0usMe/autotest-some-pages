509f6a11a6b1c4466c8c18c89c0a1a0a091c5cf7
4018KShvaykoAS <supercilious_me@vk.com>
2023-06-06 14:06:47 +0300

    using Page Object model

--- Lesson 4/test_main_page.py
+++ Lesson 4/test_main_page.py
@@ -1,10 +1,8 @@
-from selenium.webdriver.common.by import By
+from pages.main_page import MainPage
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link) 
-    go_to_login_page(browser) 
+    page = MainPage(browser, link)
+    page.open()
+    page.go_to_login_page() 
