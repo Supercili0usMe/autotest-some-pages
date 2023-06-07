4ce4459a6052b943de99be43dc0a6aa66b683d82
4018KShvaykoAS <supercilious_me@vk.com>
2023-06-07 19:13:47 +0300

    Marked some tests

--- Lesson 4/test_product_page.py
+++ Lesson 4/test_product_page.py
@@ -31,15 +31,15 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
-
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-
-def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
+@pytest.mark.need_review
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = BasketPage(browser, link)
     page.open()
@@ -47,9 +47,16 @@ def test_guest_can_see_product_in_basket_opened_from_product_page(browser):
     page.should_not_be_any_products()
     page.should_be_empty_basket_text()
 
+@pytest.mark.need_review
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_to_basket()
+    page.should_be_same_name()
+    page.should_be_same_price()
 
 class TestUserAddToBasketFromProductPage():   
-
     @pytest.fixture(scope="function", autouse=True)
     def setup(self, browser):
         email = f"{time.time()}@gmail.com"
@@ -65,6 +72,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser, link)
